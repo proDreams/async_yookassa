5e06688bfa13e63755bdd687b7a336efc0a98b16
prodream <sushkoos@gmail.com>
2024-10-26 23:42:48 +0400

    - Update validation for PaymentMethodData model.

--- async_yoomoney/models/payment_submodels/payment_method_data_model.py
+++ async_yoomoney/models/payment_submodels/payment_method_data_model.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 
 from async_yoomoney.models.payment_submodels.payment_method_data_submodels.articles_model import (
     Articles,
@@ -19,6 +19,7 @@ from async_yoomoney.models.payment_submodels.payment_method_data_submodels.vat_d
 class TypeEnum(str, Enum):
     sber_loan = "sber_loan"
     mobile_balance = "mobile_balance"
+    bank_card = "bank_card"
     cash = "cash"
     sbp = "sbp"
     b2b_sberbank = "b2b_sberbank"
@@ -32,7 +33,26 @@ class PaymentMethodData(BaseModel):
     type: TypeEnum
     phone: str | None = None
     card: Card | None = None
-    payment_purpose: str | None = Field(max_length=210)
+    payment_purpose: str | None = Field(max_length=210, default=None)
     vat_data: VatData | None = None
     articles: list[Articles] | None = None
     electronic_certificate: ElectronicCertificate | None = None
+
+    class Config:
+        use_enum_values = True
+
+    @model_validator(mode="before")
+    def validate_required_fields(cls, values):
+        type_value = values.get("type")
+
+        if type_value == TypeEnum.mobile_balance:
+            if not values.get("phone"):
+                raise ValueError("Field 'phone' is required for type 'mobile_balance'")
+
+        elif type_value == TypeEnum.b2b_sberbank:
+            if not values.get("payment_purpose"):
+                raise ValueError("Field 'payment_purpose' are required for type 'b2b_sberbank'")
+            if not values.get("vat_data"):
+                raise ValueError("Field 'vat_data' are required for type 'b2b_sberbank'")
+
+        return values
