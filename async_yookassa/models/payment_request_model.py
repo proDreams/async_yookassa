9813acf3db7c0d2eb28ab67204d45941abfdd286
prodream <sushkoos@gmail.com>
2024-10-28 15:43:57 +0400

    - Added httpx dependency - Update validation in PaymentRequest model - Added custom exception classes

--- async_yookassa/models/payment_request_model.py
+++ async_yookassa/models/payment_request_model.py
@@ -1,7 +1,9 @@
+from datetime import datetime, timedelta
 from typing import Any
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 
+from async_yookassa.enums.payment_method_data_enums import PaymentMethodDataTypeEnum
 from async_yookassa.models.payment_submodels.airline_model import Airline
 from async_yookassa.models.payment_submodels.amount_model import Amount
 from async_yookassa.models.payment_submodels.confirmation_model import Confirmation
@@ -33,3 +35,44 @@ class PaymentRequest(BaseModel):
     deal: Deal | None = None
     merchant_customer_id: str | None = Field(max_length=200, default=None)
     receiver: Receiver | None = None
+
+    @model_validator(mode="before")
+    def validate_data(cls, values):
+        amount = values.get("amount")
+        if amount is None or amount.value <= 0.0:
+            raise ValueError("Invalid or unspecified payment amount")
+
+        receipt = values.get("receipt")
+        if receipt and receipt.has_items:
+            if not (receipt.email or receipt.phone):
+                raise ValueError("Both email and phone values are empty in receipt")
+            if receipt.tax_system_code is None:
+                for item in receipt.items:
+                    if item.vat_code is None:
+                        raise ValueError("Item vat_code and receipt tax_system_code not specified")
+
+        payment_token = values.get("payment_token")
+        payment_method_id = values.get("payment_method_id")
+        payment_method_data = values.get("payment_method_data")
+
+        if payment_token:
+            if payment_method_id:
+                raise ValueError("Both payment_token and payment_method_id values are specified")
+            if payment_method_data:
+                raise ValueError("Both payment_token and payment_method_data values are specified")
+        elif payment_method_id and payment_method_data:
+            raise ValueError("Both payment_method_id and payment_method_data values are specified")
+
+        if payment_method_data and payment_method_data.type == PaymentMethodDataTypeEnum.bank_card:
+            card = payment_method_data.card
+            if card:
+                date_now = datetime.now() - timedelta(hours=27)
+                date_expiry = (
+                    datetime(year=int(card.expiry_year), month=int(card.expiry_month), day=1)
+                    + timedelta(days=31)
+                    - timedelta(days=1)
+                )
+                if date_now >= date_expiry:
+                    raise ValueError("Card expired")
+
+        return values
